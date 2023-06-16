981297f8d93d9a6c8e67b87cdb8976baead091bf
Tyree <tyreewhite01102006@gmail.com>
2023-06-16 12:49:23 -0500

    Add content for home page module

--- src/home.js
+++ src/home.js
@@ -26,7 +26,32 @@ function createRestaurantHomePage() {
 
     const content = document.createElement('div');
     content.setAttribute('id', 'content');
-    body.appendChild(content)
+    content.classList.add('home');
+    body.appendChild(content);
+
+    const aboutDiv = document.createElement('div');
+    const hoursDiv = document.createElement('div');
+    const locationDiv = document.createElement('div');
+
+    content.appendChild(aboutDiv);
+    content.appendChild(hoursDiv);
+    content.appendChild(locationDiv);
+
+    aboutDiv.textContent = 'About us';
+    hoursDiv.textContent = 'Hours';
+    locationDiv.textContent = 'Location';
+
+    const aboutInfo = document.createElement('p');
+    aboutInfo.textContent = "Benvenuto a Medici! Welcome to Medici's where we serve the finest Italian food in the world from the most renowned Italian chefs. Medici's has been the origin to 100s of world renowned chefs, as well signature foods since it's founding in 1502. Some of our classics are the Spaghetti All'Assassina(Assassin's Spaghetti), Italian Rataouille, and our Italian Meat Lover's Pizza."
+    aboutDiv.appendChild(aboutInfo);
+
+    const hoursInfo = document.createElement('p');
+    hoursInfo.textContent = 'Monday: 8am - 10pm \n\r Tuesday: 8am - 10pm\nWednesday: 8am - 10pm\nThursday: 8am - 10pm\nFriday: 8am - 12pm\nSaturday: 8am - 12pm\nSunday: 8am - 12pm';
+    hoursDiv.appendChild(hoursInfo);
+
+    const locationInfo = document.createElement('p');
+    locationInfo.textContent = 'Piazzale degli Medici, 6. 50122, Firenze Fl, Italy';
+    locationDiv.appendChild(locationInfo);
 }
 
 export default createRestaurantHomePage;
