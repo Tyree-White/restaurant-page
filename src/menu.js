08864dc3c8538848e104e62eb29099c14395c40a
Tyree <tyreewhite01102006@gmail.com>
2023-06-17 12:39:01 -0500

    Add menu item descriptions

--- src/menu.js
+++ src/menu.js
@@ -52,9 +52,9 @@ function createRestaurantMenuPage() {
 
     menuItem1.textContent = 'Spaghetti';
     menuItem2.textContent = "Spaghetti all'Assassina";
-    menuItem3.textContent = 'Chicken Parmesean';
+    menuItem3.textContent = 'Chicken Parmigiano';
     menuItem4.textContent = "Braised Chicken all'Arrabbiata";
-    menuItem5.textContent = 'Steak da Firenze';
+    menuItem5.textContent = 'Bistecca alla Fioretina';
     menuItem6.textContent = 'Baked Mostaccioli';
     menuItem7.textContent = 'Chicken Picatta';
     menuItem8.textContent = 'Italian Ratatouille';
@@ -82,7 +82,34 @@ function createRestaurantMenuPage() {
     menuItem7.appendChild(chickenPicatta);
     const italianRatatouille = document.createElement('img');
     italianRatatouille.src = 'img/italianratat.jpg';
-    menuItem8.appendChild(italianRatatouille)
+    menuItem8.appendChild(italianRatatouille);
+
+    const menuDesc1 = document.createElement('p');
+    const menuDesc2 = document.createElement('p');
+    const menuDesc3 = document.createElement('p');
+    const menuDesc4 = document.createElement('p');
+    const menuDesc5 = document.createElement('p');
+    const menuDesc6 = document.createElement('p');
+    const menuDesc7 = document.createElement('p');
+    const menuDesc8 = document.createElement('p');
+
+    menuDesc1.textContent = 'Spaghetti with meatballs, tomato sauce, parmesean cheese, and a hint of chili powder.';
+    menuDesc2.textContent = "The Assassin's Spaghetti is a spin on the signature classic. Pan cooked spaghetti with a texture different to that of normal spaghetti with added chili peppers to give an extra kick.";
+    menuDesc3.textContent = 'Breaded chicken top with tomato sauce and mozzarella, as well as provolone cheese, w/ the added option of bacon or ham.';
+    menuDesc4.textContent = 'Braised chicken spiced with poblano, and chile flakes to add a fiery kick. Served on top of polenta with a tomato sauce drizzled on top.';
+    menuDesc5.textContent = 'A hefty 3 finger thick T-bone steak from the highest quality of cattle, lightly seasoned with salt and pepper, and cooked rare.';
+    menuDesc6.textContent = 'Penne pasta layered with a mixture of ricotta, mozzarella, parmesesan, and tomato sauce, sprinkled with salt and pepper, and cooked to a golden brown crisp.';
+    menuDesc7.textContent = 'Browned chicken breasts, with cooked wine and butter served on top as sauce.';
+    menuDesc8.textContent = 'An Italian spin on Ratatouille. Sliced zucchini, squash, onion, and eggplant. Served with tomato sauce and potatoes, with an italian loaf on the side.';
+
+    menuItem1.appendChild(menuDesc1);
+    menuItem2.appendChild(menuDesc2);
+    menuItem3.appendChild(menuDesc3);
+    menuItem4.appendChild(menuDesc4);
+    menuItem5.appendChild(menuDesc5);
+    menuItem6.appendChild(menuDesc6);
+    menuItem7.appendChild(menuDesc7);
+    menuItem8.appendChild(menuDesc8);
 }
 
 export default createRestaurantMenuPage;
