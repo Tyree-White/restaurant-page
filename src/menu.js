00534202cc3178063bd667098257457ec25bdcfb
Tyree <tyreewhite01102006@gmail.com>
2023-06-17 16:08:19 -0500

    Change font size of menu items

--- src/menu.js
+++ src/menu.js
@@ -94,12 +94,12 @@ function createRestaurantMenuPage() {
     const menuDesc8 = document.createElement('p');
 
     menuDesc1.textContent = 'Spaghetti with meatballs, tomato sauce, parmesean cheese, and a hint of chili powder.';
-    menuDesc2.textContent = "The Assassin's Spaghetti is a spin on the signature classic. Pan cooked spaghetti with a texture different to that of normal spaghetti with added chili peppers to give an extra kick.";
+    menuDesc2.textContent = "Pan cooked spaghetti with a texture different to that of normal spaghetti with added chili peppers to give an extra kick.";
     menuDesc3.textContent = 'Breaded chicken top with tomato sauce and mozzarella, as well as provolone cheese, w/ the added option of bacon or ham.';
     menuDesc4.textContent = 'Braised chicken spiced with poblano, and chile flakes to add a fiery kick. Served on top of polenta with a tomato sauce drizzled on top.';
     menuDesc5.textContent = 'A hefty 3 finger thick T-bone steak from the highest quality of cattle, lightly seasoned with salt and pepper, and cooked rare.';
     menuDesc6.textContent = 'Penne pasta layered with a mixture of ricotta, mozzarella, parmesesan, and tomato sauce, sprinkled with salt and pepper, and cooked to a golden brown crisp.';
-    menuDesc7.textContent = 'Browned chicken breasts, with cooked wine and butter served on top as sauce.';
+    menuDesc7.textContent = 'Browned chicken breasts, sauced with cooked wine and butter served on top.';
     menuDesc8.textContent = 'An Italian spin on Ratatouille. Sliced zucchini, squash, onion, and eggplant. Served with tomato sauce and potatoes, with an italian loaf on the side.';
 
     menuItem1.appendChild(menuDesc1);
